706fa1900294ebed791a02c6d7260bcec51bfff5
lucasszulansky <lucas.szulansky@gmail.com>
2023-08-02 16:22:49 -0700

    August 2nd study

---
+++ question5.rb
@@ -0,0 +1,8 @@
+x = 10
+
+def add_five(x)
+  x = x + 5
+end
+
+add_five(x)
+puts x
--- scratch.rb
+++ scratch.rb
@@ -1,11 +1,6 @@
-def dot_separated_ip_address?(input_string)
-  dot_separated_words = input_string.split(".")
-  return false unless dot_separated_words.size == 4
+numbers = [7, 9, 13, 25, 18]
 
-  while dot_separated_words.size > 0 do
-    word = dot_separated_words.pop
-    return false unless is_an_ip_number?(word)
-  end
-
-  true
+until numbers.size == 0
+  puts numbers.shift
 end
+
